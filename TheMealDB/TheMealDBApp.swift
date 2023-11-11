7a193f321049d8df264c10dbf029741f1cd80f63
Rachel <lecesne.rachellois@yahoo.com>
2023-11-11 16:06:45 -0600

    Initial Commit

---
+++ TheMealDB/TheMealDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMealDBApp.swift
+//  TheMealDB
+//
+//  Created by Madison Perniciaro on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TheMealDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
